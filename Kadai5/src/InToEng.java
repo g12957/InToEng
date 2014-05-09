f09a4cfed5ef132f32c23d62e8e88aa44e3cce73
g12961ym <g12961ym+git@s20614.tsuda.ac.jp>
2014-05-09 12:23:35 +0900

    山崎茉莉ドライバー 土屋桃子ナビゲーター 1000~10000追加

--- Kadai5/src/InToEng.java
+++ Kadai5/src/InToEng.java
@@ -12,19 +12,33 @@ import java.util.Scanner;
 	    
 	    // 数値を英訳する変換するメソッド
 	    static String translateEng(int n) {
-	    	String[] num = {"zero","one","two","three","four","five","six",
+	    	int x,y,z;	    	
+	    	
+	    	if(n==0) return "zero";
+	    	String[] num = {"","one","two","three","four","five","six",
 	    			"seven","eight","nine","ten","eleven","twelve","thirteen",
 	    			"fourteen","fifteen","eighteen","nineteen"
 	    	};
 	    	String[] num2 = {"", "", "twenty","thirty","forty","fifty","sixty",
 	    			"seventy","eighty","ninety","hundred"};
-	    	
-	    	if(n>=20 && n<100) {
-	    		int x = n/10;
-	    		String y = num[n-10*x];
-	    		if(y.equals("zero")) return num2[x];
-	    		return num2[x]+" "+num[n-10*x];
+
+	    	if(n>=1000 && n<10000) {
+	    		x = n/1000;
+	    		y = (n-1000*x)/100;
+	    		z = (n-1000*x-100*y)/10;
+	    		return num[x]+" thousand "+num[y]+" "+num2[10]+" "+num2[z]+" "+num[n-1000*x-100*y-10*z];
 	    	}
+	    	else if(n>=100 && n<1000) {
+	    		x = n/100;
+	    		y = (n-100*x)/10;
+	    		return num[x]+" "+num2[10]+" "+num2[y]+" "+num[n-100*x-10*y];
+	    	}	 
+	    	else if(n>=20 && n<100) {
+	    		x = n/10;
+	    		y = n-10*x;
+	    		return num2[x]+"  "+num[y];
+	    	}	    	
+   	
 	    		return num[n];
 	    }
 	}
